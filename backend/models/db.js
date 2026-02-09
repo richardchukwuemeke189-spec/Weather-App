02e589e81eb27ae331df881fe79cadb6717e9222
ChukwuR <chukwur919@gmail.com>
2026-02-09 11:45:24 +0100

    Updated db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -6,11 +6,20 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10, queueLimit: 0
+});
+
+pool.getConnection()
+  .then(conn => {
+    console.log("✅ MySQL connected successfully");
+    conn.release();
+  })
+  .catch(err => {
+    console.error("❌ MySQL error:", err.message);
 });
 
 module.exports = pool;
