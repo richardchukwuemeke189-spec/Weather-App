01a6728cbb7e539f030c936c59f7d99519fedfc7
ChukwuR <chukwur919@gmail.com>
2026-02-25 16:09:29 +0100

    Updated db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,15 +1,18 @@
+// db.js
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || null,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 5432,
-  max: 10, // connection pool size
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+  max: 10,
 });
 
 module.exports = pool;
