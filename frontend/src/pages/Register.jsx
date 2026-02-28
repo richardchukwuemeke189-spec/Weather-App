8bfa03d33e6fcd67315199842ccd692e2f3d6782
ChukwuR <chukwur919@gmail.com>
2026-02-28 08:57:18 +0100

    Updated Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -36,7 +36,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/register`,
+        `${import.meta.env.VITE_API_URL}/api/auth/register`,
         data,
         { headers: { 'Content-Type': 'multipart/form-data' } }
       );
